2cb032af08360b0254b697ed2edd2f67f28b1aaa
Aman Aggarwal <116307103+amanaggarwal06@users.noreply.github.com>
2023-08-22 13:01:08 +0530

    Added solution - LeetHub

--- Count Reverse Pairs - GFG/count-reverse-pairs.cpp
+++ Count Reverse Pairs - GFG/count-reverse-pairs.cpp
@@ -9,39 +9,34 @@ using namespace std;
 // User function Template for C++
 
 class Solution {
-  public:
-  
-  void merge( vector<int> &arr, int low, int mid, int high){
-      
-      int first = low, second = mid+1;
-      vector<int> temp;
-      
-      while(first <= mid && second <= high){
-          
-          if(arr[first] <= arr[second]){
-              temp.push_back(arr[first++]);
-          }
-          else{ // first > second
-              temp.push_back(arr[second++]);
-          }
-      }
-      
-      while(first <= mid){
-          temp.push_back(arr[first++]);
-      }
-      
-      while(second <= high){
-          temp.push_back(arr[second++]);
-      }
-      
-      //putting sorted elements in the given array
-      for(int i=low; i<= high; i++){
-          arr[i] = temp[i-low];
-      }
-      
-  }
-  
-  int CountPairs(vector<int> &arr, int s, int mid, int e){
+    
+    void merge(vector<int> &arr, int s, int mid, int e){
+        
+        vector<int> temp;
+        int i = s, j = mid+1;
+        
+        while(i<= mid && j<= e){
+            if(arr[i] <= arr[j]){
+                temp.push_back(arr[i++]);
+            }
+            else{
+                temp.push_back(arr[j++]);
+            }
+        }
+        
+        while(i<=mid){
+            temp.push_back(arr[i++]);
+        }
+        while(j<= e){
+            temp.push_back(arr[j++]);
+        }
+        
+        for(int i = s; i<= e; i++){
+            arr[i] = temp[i-s];
+        }
+    }
+    
+     int CountPairs(vector<int> &arr, int s, int mid, int e){
 
       int cnt = 0, right = mid+1;
 
@@ -56,29 +51,32 @@ class Solution {
       }
       return cnt;
   }
-
-  int mergeSort(vector<int> &arr, int s, int e){
     
-    int cnt = 0;
-    //base case
-    if(s>=e){
+    int mergeSort(vector<int> &arr, int s, int e){
+        
+        //base case
+        if(s>=e){
+            return 0;
+        }
+        
+        //processing
+        int cnt = 0;
+        int mid = s+(e-s)/2;
+        
+        //recursive calls
+        cnt += mergeSort(arr, s, mid);
+        cnt += mergeSort(arr, mid+1, e);
+        
+        cnt += CountPairs(arr, s, mid, e);
+        merge(arr, s, mid, e);
+        
         return cnt;
     }
     
-    int mid = s+(e-s)/2;
-    
-    //recursive case
-    cnt += mergeSort(arr, s, mid);
-    cnt += mergeSort(arr, mid+1, e);
-    
-    cnt += CountPairs(arr, s, mid, e);
-    merge(arr, s, mid, e);
-    
-    return cnt;
-    
-  }
+  public:
     int countRevPairs(int n, vector<int> arr) {
         // Code here
+        
         return mergeSort(arr, 0, arr.size()-1);
     }
 };

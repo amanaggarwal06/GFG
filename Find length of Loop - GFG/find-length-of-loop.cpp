e55ea38ce43506b98b3ea8c87d52dd1672ba05d7
Aman Aggarwal <116307103+amanaggarwal06@users.noreply.github.com>
2023-09-04 17:43:54 +0530

    Added solution - LeetHub

---
+++ Find length of Loop - GFG/find-length-of-loop.cpp
@@ -0,0 +1,124 @@
+//{ Driver Code Starts
+// driver code
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void loopHere(Node* head, Node* tail, int position)
+{
+    if(position==0) return;
+    
+    Node* walk = head;
+    for(int i=1; i<position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+int countNodesinLoop(Node* head);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, num;
+        cin>>n;
+        
+        Node *head, *tail;
+        cin>> num;
+        head = tail = new Node(num);
+        
+        for(int i=0 ; i<n-1 ; i++)
+        {
+            cin>> num;
+            tail->next = new Node(num);
+            tail = tail->next;
+        }
+        
+        int pos;
+        cin>> pos;
+        loopHere(head,tail,pos);
+        
+        cout<< countNodesinLoop(head) << endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
+
+
+/*
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+//Function to find the length of a loop in the linked list.
+
+Node* isCycle(Node* head){
+    
+    if(head == NULL || head->next == NULL){
+        return NULL;
+    }
+    else if(head->next == head){
+        return head;
+    }
+    
+    Node* slow = head;
+    Node* fast = head;
+    
+    while(fast != NULL){
+        fast = fast->next;
+        
+        if(fast != NULL){
+            fast = fast->next;
+        }
+        
+        slow = slow->next;
+        
+        if(slow == fast){
+            return slow;
+        }
+    }
+    return NULL;
+}
+
+int countNodesinLoop(struct Node *head)
+{
+    // Code here
+    
+    Node* intersect = isCycle(head);
+    
+    if(intersect == NULL){
+        return 0;
+    }
+    
+    Node* temp = intersect->next; int cnt = 1;
+    
+    while(temp != intersect){
+        cnt++;
+        temp = temp->next;
+    }
+    
+    return cnt;
+}

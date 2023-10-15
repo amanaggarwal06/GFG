d3e965179484db05e3be8f42a8f8b0b7d4cb8d5c
Aman Aggarwal <116307103+amanaggarwal06@users.noreply.github.com>
2023-10-15 11:48:30 +0530

    Added solution - LeetHub

---
+++ First negative integer in every window of size k - GFG/first-negative-integer-in-every-window-of-size-k.cpp
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<long long> printFirstNegativeInteger(long long int arr[],
+                                             long long int n, long long int k);
+
+// Driver program to test above functions
+int main() {
+    long long int t, i;
+    cin >> t;
+    while (t--) {
+        long long int n;
+        cin >> n;
+        long long int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        long long int k;
+        cin >> k;
+
+        vector<long long> ans = printFirstNegativeInteger(arr, n, k);
+        for (auto it : ans) cout << it << " ";
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+vector<long long> printFirstNegativeInteger(long long int A[],
+                                             long long int N, long long int K) {
+                            
+        
+        vector<long long> ans;
+        queue<long long> q;
+        
+        for(int i=0; i<K; i++){
+            
+            if(A[i] < 0){
+                q.push(i);
+            }
+        }
+        
+        if(q.empty()){
+            ans.push_back(0);
+        }
+        else{
+            ans.push_back(A[q.front()]);
+        }
+        
+        for(int i = K; i<N; i++){
+            
+            //remove previous element
+            if( !q.empty() && ((i - q.front()) >= K )){
+                q.pop();
+            }
+                //push into queue
+            if(A[i] < 0){
+                q.push(i);
+                
+            }
+            
+            //getting answer
+            if(q.empty()){
+                ans.push_back(0);
+            }
+            else{
+                ans.push_back(A[q.front()]);
+            }
+            
+        }
+        return ans;
+        
+        
+        
+ }
